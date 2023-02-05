6c63b7af870f6f11ffc651eab3a08c433ae2c1ba
minorsetback <vlad232362@gmail.com>
2023-02-05 21:04:01 +0200

    fix

--- src/components/cart/CartCheckout.tsx
+++ src/components/cart/CartCheckout.tsx
@@ -1,9 +1,11 @@
 import { GlobalContext } from "@/context/Global.context";
 import style from "@/styles/cart/CartCheckout.module.scss";
+import { useRouter } from "next/router";
 import { useContext } from "react";
 
 const CartCheckout = () => {
   const { cart, promo, form } = useContext(GlobalContext);
+  const router = useRouter();
   const calc = (discount?: number) => {
     let price = form.delivery;
     cart.map((item) => {
@@ -21,7 +23,9 @@ const CartCheckout = () => {
       <div className={style.checkout__block}>
         <span className={style.checkout__title}>Shipping</span>
         <span className={style.checkout__price}>
-          ${form.delivery.toFixed(2)}
+          {router.pathname === "/"
+            ? "calculated next step"
+            : "$" + form.delivery.toFixed(2)}
         </span>
       </div>
       <div className={style.checkout__block}>
@@ -38,7 +42,7 @@ const CartCheckout = () => {
           <span className={style.checkout__currency}>AUD</span>
           <span className={style.checkout__totalPrice}>
             {promo.includes("GETDISCOUNT")
-              ? calc(0.05).toFixed(2) 
+              ? calc(0.05).toFixed(2)
               : calc().toFixed(2)}
           </span>
         </div>
